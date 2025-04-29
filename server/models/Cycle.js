6e3b6d7e4f109c1da172a6ce15cfafb17e7e2af3
akram ahil <akram.ahil123@gmail.com>
2025-04-29 12:17:25 +0100

    Update configuration files and enhance project structure for improved organization.

---
+++ server/models/Cycle.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const CycleSchema = new mongoose.Schema({
+  
+  class: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Class',
+    required: true
+  },
+  name: {
+    type: String,
+    enum: ['Maternelle', 'Primaire', 'College', 'Lycee'],
+    required: true
+  },
+  academicYear: {
+    type: String,
+    required: [true, 'Please add academic year']
+  },
+  remarks: {
+    type: String
+  },
+  examDate: {
+    type: Date
+  },
+  schoolId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'School',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+
+module.exports = mongoose.model('Grade', CycleSchema); 
