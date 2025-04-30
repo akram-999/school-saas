14bd6bf49c2e501bcd25b2a2d277344e16693cfe
akram ahil <akram.ahil123@gmail.com>
2025-04-30 09:53:07 +0100

    activity v1

---
+++ server/models/Activity.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const activitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student',
+    },
+    schoolId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'School',
+    },
+});
+
+const Activity = mongoose.model('Activity', activitySchema);
+
+module.exports = Activity;
