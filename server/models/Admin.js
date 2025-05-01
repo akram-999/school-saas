45e551f14987a328233388ef0a67ca3a8aaba479
akram ahil <akram.ahil123@gmail.com>
2025-05-01 16:04:14 +0100

    admin model v2

---
+++ server/models/Admin.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+    firstName: {type: String, required:true},
+    lastName: {type: String, required:true},
+    email: {type: String, unique: true},
+    rol: {type: String, default: 'admin' },
+    password: {type: String, required:true}
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
+
