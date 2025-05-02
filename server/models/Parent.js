bb509098e9ce92b5cc175f042f45704edd2b9c1c
akram ahil <akram.ahil123@gmail.com>
2025-05-02 18:44:20 +0100

    Refactor Parent model to replace name fields with firstName and lastName; add required fields for address and role

--- server/models/Parent.js
+++ server/models/Parent.js
@@ -1,11 +1,37 @@
 import mongoose from 'mongoose';
 
 const parentSchema = new mongoose.Schema({
-  name: String,
-  name_ar: String,
+  firstName: {
+    type:String,
+    required:true,
+  },
+  firstName_ar: {
+    type:String,
+    required:true,
+  },
+  lastName:{
+    type:String,
+    required:true,
+  },
+  lastName_ar:{
+    type:String,
+    required:true,
+  },
+  cin:{
+    type:String,
+    required:true,
+  },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   phone: String,
+  address:{
+    type:String,
+    required:true,
+  },
+  rol:{
+    type:String,
+    default:'parent'
+  },
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
   schoolId: { type: mongoose.Schema.Types.ObjectId, ref: 'School' },
 }, { timestamps: true });
